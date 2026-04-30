383e949ca648ec911871933c35a240ee1075a903
JongYeonHwang <junyon6@gmail.com>
2026-04-30 23:22:40 +0900

    lambda에서 DB연결실패

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import { Express } from 'express';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -10,6 +15,19 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance() as Express;
+  return serverlessExpress({ app: expressApp }) as Handler;
+  // await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+): Promise<any> => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
+// bootstrap();
